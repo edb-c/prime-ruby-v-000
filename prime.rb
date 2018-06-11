bc1f4cac19c864a723c414c303e735729d3120a3
Ethel C <cofresi.ethel@gmail.com>
2018-06-11 17:26:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,14 @@
 This methond accepts a number, and checks whether it is divisible by x (every number from 2 to 1-minus that number) . If there is a number that divides into it other than 1, then return false.
 =end
 def prime?(num)
-is_prime = true
-  for x in 2..(num-1)
-    if num % x == 0
-       return is_prime = false #not prime
-       break
-    end 
-  end 
- return is_prime           
+  if num > 0
+    is_prime = true
+      for x in 2..(num-1)
+        if num % x == 0
+          return is_prime = false #not prime
+        break
+        end 
+      end 
+    return is_prime           
+  end
 end
