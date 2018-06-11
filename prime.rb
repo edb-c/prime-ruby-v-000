f8aee90eb22f9db2b9d3b257b640a198e66bf652
Ethel C <cofresi.ethel@gmail.com>
2018-06-11 17:30:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,5 @@
-=begin
-This methond accepts a number, and checks whether it is divisible by x (every number from 2 to 1-minus that number) . If there is a number that divides into it other than 1, then return false.
-=end
-is_prime = true
 def prime?(num)
+is_prime = true
   if num > 0
       for x in 2..(num-1)
         if num % x == 0
